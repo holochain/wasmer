96859d151e837b69d0d094759c8573fccc82b6a7
neonphog <neonphog@gmail.com>
2024-08-27 15:40:52 -0600

    fix compiler-llvm

--- hc-publish.sh
+++ hc-publish.sh
@@ -6,7 +6,7 @@ cargo publish --manifest-path lib/types/Cargo.toml && \
   cargo publish --manifest-path lib/derive/Cargo.toml && \
   cargo publish --manifest-path lib/compiler/Cargo.toml && \
   cargo publish --manifest-path lib/compiler-cranelift/Cargo.toml && \
-  cargo publish --manifest-path lib/compiler-llvm/Cargo.toml && \
+  cargo publish --manifest-path lib/compiler-llvm/Cargo.toml --no-verify && \
   cargo publish --manifest-path lib/compiler-singlepass/Cargo.toml && \
   cargo publish --manifest-path lib/api/Cargo.toml && \
   cargo publish --manifest-path lib/cache/Cargo.toml && \
