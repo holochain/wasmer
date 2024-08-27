695292a9ea14f661328dabba212fad09e9f49595
neonphog <neonphog@gmail.com>
2024-08-27 15:29:24 -0600

    bump versions

---
+++ hc-publish.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+cargo publish --manifest-path lib/types/Cargo.toml && \
+  cargo publish --manifest-path lib/vm/Cargo.toml && \
+  cargo publish --manifest-path lib/object/Cargo.toml && \
+  cargo publish --manifest-path lib/derive/Cargo.toml && \
+  cargo publish --manifest-path lib/compiler/Cargo.toml && \
+  cargo publish --manifest-path lib/compiler-cranelift/Cargo.toml && \
+  cargo publish --manifest-path lib/compiler-llvm/Cargo.toml && \
+  cargo publish --manifest-path lib/compiler-singlepass/Cargo.toml && \
+  cargo publish --manifest-path lib/api/Cargo.toml && \
+  cargo publish --manifest-path lib/cache/Cargo.toml && \
+  cargo publish --manifest-path lib/middlewares/Cargo.toml
